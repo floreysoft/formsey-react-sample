7769c42f7fd5261f3e85b58ad63351dba9b8fe41
Alexander Brickwedde <alex_git@brickwedde.de>
2020-07-02 11:45:47 +0200

    More stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
-import '@formsey/core';
-import '@formsey/fields-native';
 import { FormseyForm } from '@formsey/react';
 
 
 function App() {
+  var formref = useRef(null);
   var changeHandler = function clickhandler(detail) {
     console.log(detail);
+    if (detail.name === "zeit") {
+      formref.current.setValue("datum", "2020-01-01");
+    }
   };
+
+  var url = "https://cdn.formsey.com/DHwdsd3s5IhNDCWVslZ7/5Aw1YlRXCiIAlSqNmK7s";
 
   return (
     <div className="App">
       <header className="App-header">
-        <FormseyForm onChange={changeHandler}></FormseyForm>
+        <FormseyForm ref={formref} onChange={changeHandler} src={url}></FormseyForm>
       </header>
     </div>
   );
